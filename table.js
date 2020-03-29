d13b88188e00c828491e2f17380786d94c8efa0c
Akhtar Hussain <48026083+akhtar02@users.noreply.github.com>
2020-03-29 22:20:51 +0500

    Create table.js

---
+++ table.js
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script>
+        var table ;
+
+        table = prompt("Enter Your Number Here");
+
+            document.write(table+" x 1 = "+table);
+            document.write("<br>"+table+" x 2 = "+table*2);
+            document.write("<br>"+table+" x 3 = "+table*3);
+            document.write("<br>"+table+" x 4 = "+table*4);
+            document.write("<br>"+table+" x 5 = "+table*5);
+            document.write("<br>"+table+" x 6 = "+table*6);
+            document.write("<br>"+table+" x 7 = "+table*7);
+            document.write("<br>"+table+" x 8 = "+table*8);
+            document.write("<br>"+table+" x 9 = "+table*9);
+            document.write("<br>"+table+" x 10 = "+table*10);
+        
+
+    
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
